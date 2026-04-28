848749fc61bbd11c1db7ba7da24cc6983f03d367
Joel MITONDO <mitondomohindo@gmail.com>
2026-04-28 19:13:03 +0100

    fix(contact): fix contact form bug for iPhone users

--- SCRIPTS/formContact.js
+++ SCRIPTS/formContact.js
@@ -18,19 +18,29 @@ formContact.addEventListener('submit', function(e) {
     const numero = "243823310484";
 
    
-    const texteFinal = `Bonjour Joel MITONDO, %0Aje suis ${prenom} ${nom}.%0A*Sujet :* ${sujet}%0A%0A *Message :*%0A${message} %0A%0A*Mon Email :* ${email}`;
+    const texteFinal = `Bonjour Joel MITONDO, %0A%0AJe suis ${prenom} ${nom}.%0A%0A*Sujet :* ${sujet}%0A%0A *Message :*%0A${message} %0A%0A*Mon Email :* ${email}`;
     const lienWhatsApp = `https://wa.me/${numero}?text=${texteFinal}`;
 
-    // Délai de 2 secondes avant la redirection
-    setTimeout(function() {
-        window.open(lienWhatsApp, '_blank');
-        
-        // Optionnel : recacher la notification après le départ
-        setTimeout(() => {
-            notification.classList.add('hidden');
-            notification.classList.remove('flex');
-        }, 3000);
-        
-    }, 4000); 
+    // Pour iPhone, on réduit le délai au minimum ou on change la méthode
+        setTimeout(function() {
+            // Détection de l'iPhone / iOS
+            const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+            if (isIOS) {
+                // Sur iPhone, on redirige directement dans l'onglet actuel pour éviter le blocage
+                window.location.href = lienWhatsApp;
+            } else {
+                // Sur Android/PC, on peut ouvrir un nouvel onglet
+                window.open(lienWhatsApp, '_blank');
+            }
+
+            // la notification sois cachée après
+            setTimeout(() => {
+                notification.classList.add('hidden');
+            }, 500);
+            
+        }, 3500); 
+
+    
 });
 
